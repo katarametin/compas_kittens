ebf2e9f605518eb4b3c94b20ccc4c5e91592d113
Katerina <78984454+katarametin@users.noreply.github.com>
2023-05-11 17:12:33 +0200

    add after_rhino_install plugin

--- src/compas_kittens/rhino/install.py
+++ src/compas_kittens/rhino/install.py
@@ -1,6 +1,27 @@
+import os
+
 from compas.plugins import plugin
+from compas_ghpython.components import install_userobjects
 
 
 @plugin(category="install")
 def installable_rhino_packages():
     return ["compas_kittens"]
+
+
+@plugin(category="install")
+def after_rhino_install(installed_packages):
+    import compas_kittens
+
+    install_folder = os.path.dirname(compas_kittens.__file__)
+    userobjects = os.path.join(install_folder, "ghpython", "components", "ghuser")
+
+    installed_objects = install_userobjects(userobjects)
+
+    return [
+        (
+            "compas_kittens",
+            "Installed {} GH User Objects".format(len(installed_objects)),
+            True,
+        )
+    ]
